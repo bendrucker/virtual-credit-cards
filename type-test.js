c18c1303922ccba7f1f15f1c44781062eb8b08d3
Ben Drucker <bvdrucker@gmail.com>
2016-06-16 15:05:26 -0700

    Add test for Android specific input type

---
+++ type-test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var test = require('tape')
+var proxyquire = require('proxyquire')
+
+test('input type', function (t) {
+  t.equal(proxyquire('./type', {'is-android': true}), 'tel')
+  t.equal(proxyquire('./type', {'is-android': false}), 'text')
+  t.end()
+})
